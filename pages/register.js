b91bb7c2e8df772a06f7b06c5033006d28f5b69d
geekkweeks <zahgreat30@gmail.com>
2022-01-12 10:34:35 +0700

    overlay

--- pages/register.js
+++ pages/register.js
@@ -1,52 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function RegisterPage() {
+  const [field, setField] = useState({});
+  const [progress, setProgress] = useState(false);
 
-    const [field, setField] = useState({});
+  function setValue(e) {
+    const target = e.target;
 
-    function setValue(e){
-        const target = e.target;
+    const name = target.name;
+    const value = target.value;
 
-        const name = target.name;
-        const value = target.value;
+    setField({
+      ...field,
+      [name]: value,
+    });
 
-        setField({
-            ... field,
-            [name]: value
-        })
+    console.log(field);
+  }
 
-        console.log(field);
-    }
+  async function doRegister(e) {
+    e.preventDefault();
+    setProgress(true);
+    const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(field),
+    });
+    const res = await req.json();
 
-    async function doRegister(e){
-        e.preventDefault();
-        const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/register`,{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(field)
-        })
-        const res = await req.json();
-
-        console.log(res);
-    }
-
+    setProgress(false);
+    console.log(res);
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
       <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
         <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1>
         <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-          <form onSubmit={doRegister} className="px-5 py-7">
+          <form onSubmit={doRegister} className="px-5 py-7" relative>
+            {progress && <div className="absolute inset-0 z-10 bg-white/50" />}
+
             <label className="font-semibold text-sm text-gray-600 pb-1 block">
               Username
             </label>
             <input
               type="text"
               className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              name = "username"
-              onChange = {setValue}
+              name="username"
+              onChange={setValue}
             />
             <label className="font-semibold text-sm text-gray-600 pb-1 block">
               Password
@@ -54,8 +57,8 @@ export default function RegisterPage() {
             <input
               type="password"
               className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              name = "password"
-              onChange = {setValue}
+              name="password"
+              onChange={setValue}
             />
             <label className="font-semibold text-sm text-gray-600 pb-1 block">
               Confirm Password
@@ -63,10 +66,11 @@ export default function RegisterPage() {
             <input
               type="password"
               className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-              name = "password_confirmation"
-              onChange = {setValue}
+              name="password_confirmation"
+              onChange={setValue}
             />
             <button
+              disabled={progress}
               type="submit"
               className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
             >
